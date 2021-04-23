b2c5c81e61d053bc950cf227de31329295da7f61
guerlielton@gmail.com/usr/bin/python3 <guerlielton@gmail.com>
2021-04-23 19:16:42 -0300

    Fix: text ec2

--- terraform/terraform_files/ec2_elk.tf
+++ terraform/terraform_files/ec2_elk.tf
@@ -6,5 +6,4 @@ resource "aws_instance" "elk" {
   vpc_security_group_ids      = [aws_security_group.allow_traffic_elk.id]
   associate_public_ip_address = true
   tags                        = local.common_tags
-  #Insira o nome da instância de sua preferência.
 }
